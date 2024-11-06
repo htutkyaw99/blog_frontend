9f416ffb04b75b4aa800fe1320a92aef93461aeb
htutkyaw <htutkyaw22dec99@gmail.com>
2024-11-06 23:03:13 +0630

    Axios Instance setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { axiosInstance } from "./config/axiosConfig";
+
+function App() {
+  return (
+    <>
+      <h1 className=" bg-red-500">Hello From React</h1>
+    </>
+  );
+}
+
+export default App;
